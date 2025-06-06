5290838d684c1434fc164f115b5f32e3e4426489
Mathieu Langumier <mathieu.langumier@gmail.com>
2025-06-06 17:30:04 +0200

    Prep: created CombatManager for later

---
+++ src/rpg/services/CombatManager.java
@@ -0,0 +1,19 @@
+package rpg.services;
+
+import rpg.character.player.Player;
+
+public class CombatManager {
+    private Player player;
+    // private List<Enemy> enemies;
+
+    /**
+     * Default constructor
+     */
+    public CombatManager() {
+        // Create list of enemies
+    }
+
+    // Method startTurn() (~find better name)
+
+
+}
